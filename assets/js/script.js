20cd4c83e35c9a6c61c8e959fa4a43e45edcc0a1
Lia OBrien <liaobrien123@gmail.com>
2021-09-20 16:16:02 -0400

    set up current weather container to fill with current weather data and date for given city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,21 @@ var APIKey = "594f4b0a93a1af56a8fb27e3af8c5bc2";
 
 var searchBtn = document.getElementById("submit");
 
-var currentWeather = document.querySelector(".current-weather");
+var currentWeatherContainer = document.querySelector(".current-weather");
+
+var cityDateEl = document.getElementById("city-date");
+var currentTemp = document.getElementById("temp");
+var currentWind = document.getElementById("wind");
+var currentHumid = document.getElementById("humidity");
+var currentUVI = document.getElementById("uv-index");
+
+var currentDate = moment().format("M/DD/YYYY");
 
 function getData(event) {
       event.preventDefault();
 
       var city = document.getElementById("city").value;
-      var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+      var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
       fetch(queryUrl)
             .then(function (response) {
@@ -17,6 +25,13 @@ function getData(event) {
             })
             .then(function (data) {
                   console.log(data);
+
+                  cityDateEl.textContent = city + " - " + currentDate;
+                  currentTemp.textContent = "Temperature: " + data.main.temp + "°F";
+                  currentWind.textContent = "Wind: " + data.wind.speed + " MPH"
+                  currentHumid.textContent = "Humidity: " + data.main.humidity + "%";
+
+
             })
 }
 

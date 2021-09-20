32f9150e864553b18a5a0bc1c0a00ccc528266b1
Lia OBrien <liaobrien123@gmail.com>
2021-09-20 15:23:31 -0400

    fixed current weather api fetch, added basic styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 var APIKey = "594f4b0a93a1af56a8fb27e3af8c5bc2";
 
-var city = $("#city").val();
+var searchBtn = document.getElementById("submit");
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var currentWeather = document.querySelector(".current-weather");
+
+function getData(event) {
+      event.preventDefault();
+
+      var city = document.getElementById("city").value;
+      var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+      fetch(queryUrl)
+            .then(function (response) {
+                  console.log(response);
+                  return response.json();
+            })
+            .then(function (data) {
+                  console.log(data);
+            })
+}
+
+searchBtn.addEventListener('click', getData)
